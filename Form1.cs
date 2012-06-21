f8ee6f6f5b762634f3c7d7741489400fdebd26fd
Naram Qashat <cyberbotx@cyberbotx.com>
2012-06-20 20:39:33 -0400

    Initial Files
    Had just created a new C# project and added a little to the form,
    nothing fancy yet.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SDA_DonationTracker
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			this.StatusBarLabel.Text = "Testing...";
+		}
+
+		private void QuitMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
